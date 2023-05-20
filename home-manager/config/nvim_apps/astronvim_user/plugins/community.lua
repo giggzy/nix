732770883eb39c3e26145bf891de1b6f9925493f
Gabriel Farrell <gabriel.farrell@gmail.com>
2023-05-20 19:20:04 -0400

    Changes to my user config of Astronvim

--- home-manager/config/nvim_apps/astronvim_user/plugins/community.lua
+++ home-manager/config/nvim_apps/astronvim_user/plugins/community.lua
@@ -5,5 +5,60 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.java" },
+  -- ERROR: The following are not installing some dependencies, npm error but not sure why
+  -- { import = "astrocommunity.pack.yaml" },
+  -- { import = "astrocommunity.pack.bash" },
+  -- { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.json" },
+  -- END ERROR
+  -- Integrates copilot and cmp, uses TAB, current <C-j> is not working
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { -- further customize the options set by the community
+  --   "copilot.lua-cmp",
+  --   opts = {
+  --     suggestion = {
+  --       keymap = {
+  --         next = "<C-j>",
+  --         prev = "<C-k>",
+  --         select = "<CR>",
+  --         cancel = "<C-c>",
+  --         dismiss = "<C-e>",
+  --       },
+  --     },
+  --   },
+  -- },
+  -- { import = "astrocommunity.completion.copilot-lua" },
+  --[[
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local cmp = require "cmp"
+      return {
+        mapping = {
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-k>"] = cmp.mapping.select_previous_item(),
+          ["<C-p>"] = cmp.mapping.select_previous_item(),
+        },
+      }
+    end,
+  },
+  {
+    // FAILS, attempt to index into a nil value (global 'cmp')
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.mapping = require("astronvim.utils").list_insert_unique(opts.mapping, {
+          ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+          ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+          ["<C-k>"] = cmp.mapping.select_previous_item { behavior = cmp.SelectBehavior.Select },
+          ["<C-p>"] = cmp.mapping.select_previous_item { behavior = cmp.SelectBehavior.Select },
+      })
+    end,
+  },
+  --]]
 }
--- home-manager/config/nvim_apps/astronvim_user/plugins/core.lua
+++ home-manager/config/nvim_apps/astronvim_user/plugins/core.lua
@@ -4,19 +4,6 @@ return {
     "goolord/alpha-nvim",
     opts = function(_, opts)
       -- customize the dashboard header
-      -- opts.section.header.val = {
-      --   " █████  ███████ ████████ ██████   ██████",
-      --   "██   ██ ██         ██    ██   ██ ██    ██",
-      --   "███████ ███████    ██    ██████  ██    ██",
-      --   "██   ██      ██    ██    ██   ██ ██    ██",
-      --   "██   ██ ███████    ██    ██   ██  ██████",
-      --   " ",
-      --   "    ███    ██ ██    ██ ██ ███    ███",
-      --   "    ████   ██ ██    ██ ██ ████  ████",
-      --   "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-      --   "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-      --   "    ██   ████   ████   ██ ██      ██",
-      -- }
       opts.section.header.val = {
         "╔╗ ╔╗╔═══╗╔╗   ╔══╗ ╔═══╗╔════╗",
         "║║ ║║║╔═╗║║║   ╚╣╠╝ ║╔══╝║╔╗╔╗║",
@@ -32,15 +19,33 @@ return {
   -- { "max397574/better-escape.nvim", enabled = false },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   config = function(plugin, opts)
-  --     require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --   end,
-  -- },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      local luasnip = require "luasnip"
+      luasnip.filetype_extend("javascript", { "javascriptreact" })
+      luasnip.filetype_extend("typescript", { "typescriptreact" })
+    end,
+  },
+  --[[ NOT WORKING TRYING TO CUSTOMIZE CMP PLUGIN
+  {
+    "hrsh7th/nvim-cmp",
+    config = function(plugin, opts)
+      require "plugins.configs.nvim-cmp"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom nvim-cmp configuration such as custom snippets
+      local cmp = require "cmp"
+      cmp.setup.buffer {
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+        },
+      }
+    end,
+  }
+  --]]
   -- {
   --   "windwp/nvim-autopairs",
   --   config = function(plugin, opts)
