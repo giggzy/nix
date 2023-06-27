1b14f8d532d2474fd09e4de89490002ad5dd772c
Farrell, Gabriel <Gabriel.Farrell@nyulangone.org>
2023-06-27 16:47:51 -0400

    add toggleterm neovim plugin

--- home-manager/config/nvim_apps/astronvim_user/plugins/user.lua
+++ home-manager/config/nvim_apps/astronvim_user/plugins/user.lua
@@ -11,7 +11,24 @@ return {
   -- },
   {
     "imsnif/kdl.vim",
-    -- lazy load on kdl files
+    -- lazy load on kdl files, this config format is used by zellij
     ft = "kdl",
-  }
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    -- require("toggleterm").setup {
+    --   open_mapping = [[<c-\>]],  -- toogle terminal with ctrl-\ but not working close terminal
+    -- },
+    -- These did not work , still don't understand overriding config
+    -- opts = {
+    --   open_mapping = [[<c-\>]],  -- toogle terminal with ctrl-\ but not working close terminal
+    -- }
+    opts = function(_, opts)
+      opts.config = {
+        open_mapping = [[<c-\>]],  -- toogle terminal with ctrl-\ but not working close terminal
+        shadding_factor = 0.3,
+      }
+      return opts
+    end,
+  },
 }
