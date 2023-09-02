acf9833167c54bb986a22c2886ad194bc8c84d96
Farrell, Gabriel <Gabriel.Farrell@nyulangone.org>
2023-09-02 08:05:11 -0400

    Noice config modifications.

--- home-manager/config/nvim_apps/astronvim_user/plugins/noice.lua
+++ home-manager/config/nvim_apps/astronvim_user/plugins/noice.lua
@@ -15,13 +15,19 @@ return {
   config = function()
     require("noice").setup({
       lsp = {
-        -- hover.enabled = false,
         -- signature.enabled = false,
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
+        },
+        -- hover.enabled = false,  # this caused an error, expected closing bracket
+        hover = {
+          enabled = false,
+        },
+        signature = {
+          enabled = false,
         },
       },
       -- you can enable a preset for easier configuration
