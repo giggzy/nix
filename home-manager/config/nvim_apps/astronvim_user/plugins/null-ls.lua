7b02158f9fcfe08b3f785135f72a4cc850301cef
Farrell, Gabriel <Gabriel.Farrell@nyulangone.org>
2023-09-02 08:05:38 -0400

    null-ls modifications.

--- home-manager/config/nvim_apps/astronvim_user/plugins/null-ls.lua
+++ home-manager/config/nvim_apps/astronvim_user/plugins/null-ls.lua
@@ -2,7 +2,8 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
+    -- local diagnostics = null-ls.builtins.diagnostics
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,6 +12,9 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+      -- diagnostics.mypy.with({
+      --   extra_args = { "--ignore-missing-imports" },
+      -- })
     }
     return config -- return final config table
   end,
